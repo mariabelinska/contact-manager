2717130079f6d4b932179c9235d7987e0cd109b1
mariabelinska <mariabelinskaa@gmail.com>
2020-03-29 19:15:08 +0300

    no message

---
+++ src/services/index.js
@@ -0,0 +1,4 @@
+export { configureToast } from './configureToast';
+export { getContacts, addContact, editContact, deleteContact } from './contacts';
+export { reorder } from './reorder';
+export { getSequenceAfterAdd, getSequenceAfterDrag } from './sequences';
