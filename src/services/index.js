ac1251d9e019605defcf1da51bdd189a91fd0df3
mariabelinska <mariabelinskaa@gmail.com>
2020-03-29 19:16:26 +0300

    Revert "no message"
    This reverts commit 2717130079f6d4b932179c9235d7987e0cd109b1.

--- src/services/index.js
+++
@@ -1,4 +0,0 @@
-export { configureToast } from './configureToast';
-export { getContacts, addContact, editContact, deleteContact } from './contacts';
-export { reorder } from './reorder';
-export { getSequenceAfterAdd, getSequenceAfterDrag } from './sequences';
